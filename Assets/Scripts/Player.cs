3e8dbcdfa3f6e1149077c1027534aa86e1b84c8a
2006_V2\steev <costath@sheridancollege.ca>
2026-04-08 16:25:22 -0400

    Setup early player input, player animations, walking, attacking

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,33 +4,168 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     //set up player values & stats
-    public float pSpeed = 10;
+    public float pSpeed = 5;
+    public int eCount = 0; //how many times they can evolve
     [SerializeField] Vector2 pMovement;
     public float pAttackCD = 1.23f;
-    public float pHP;
+    public float pHP = 3;
+    public bool isRight = false;
+    public bool isAttacking = false;
 
     public AudioSource pSFX;
-    public SpriteRenderer pSpriteR;
+    public SpriteRenderer pSpriteRenderer;
+    public Animator pAnimator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += (Vector3)pMovement * pSpeed * Time.deltaTime;
+        if (isAttacking == true)
+        {
+            //no movement allowed
+        }
+        else if (isAttacking == false) 
+        {//allow player to walk
+            pMovementRestrict();
+        }
+            
     }
 
+    //disables walking
+    public void nuhUh()
+    {
+        isAttacking = true;
+    }
+    //allows for walking
+    public void yuhuh() 
+    {
+        isAttacking = false;
+    }
+
+    public void pMovementRestrict() 
+    {
+        //function for movement so update is less messy looking
+        if (transform.position.x >= 18.66)
+        {//limit player how much can travel to right side of screen
+            if (pMovement.x < 0)
+            {//can only move left
+                transform.position += (Vector3)pMovement * pSpeed * Time.deltaTime;
+            }
+            else
+            {
+                //do nothing
+            }
+
+        }
+        else if (transform.position.y >= -1.105)
+        {//limit how much player can travel up so they don't walk off background ground
+            if (pMovement.y < 0)
+            {//can only move down
+                transform.position += (Vector3)pMovement * pSpeed * Time.deltaTime;
+            }
+            else
+            {
+                //do nothing
+            }
+        }
+        else if (transform.position.y <= -3.2)
+        {//limit how much player can travel down so they don't walk off background ground
+            if (pMovement.y > 0)
+            {//can only move up
+                transform.position += (Vector3)pMovement * pSpeed * Time.deltaTime;
+            }
+            else
+            {
+                //do nothing
+            }
+        }
+        else if (transform.position.x <= -37.94)
+        {//limit player how much can travel to right side of screen
+            if (pMovement.x > 0)
+            {//can only move right
+                transform.position += (Vector3)pMovement * pSpeed * Time.deltaTime;
+            }
+            else
+            {
+                //do nothing
+            }
+        }
+        else
+        {//if not at any borders they can move without restriction
+            transform.position += (Vector3)pMovement * pSpeed * Time.deltaTime;
+        }
+    }
     public void OnMove(InputAction.CallbackContext contextMove)
     {
         //KAnimator.SetFloat("AnimState", 1f);
         pMovement = contextMove.ReadValue<Vector2>();
+        Debug.Log(pMovement);
+        //pAnimator.SetTrigger("CatWalking");
+        
 
-        /*if (contextMove.performed == true)
+        if (pMovement.x > 0)
         {
+            pSpriteRenderer.flipX = true;
+            isRight = true;
+        }
+        if (pMovement.x < 0 && isRight == true)
+        {
+            pSpriteRenderer.flipX = false;
+            isRight = false;
+        }
+
+        if (contextMove.performed == true)
+        {
+            pAnimator.SetBool("CatWalking1", true);
+            //_SpriteRenderer.flipX = true;
+            //_Animator.SetBool("IsWalkRight", true);
             //KAnimator.SetTrigger("Run");
-        }*/
+        }
+        else if (contextMove.started == true)
+        {
+            pAnimator.SetBool("CatWalking1", true);
+        }
+        else if (contextMove.canceled == true)
+        {
+            pAnimator.SetBool("CatWalking1", false);
+        }
+    }
+
+    public void OnInteract(InputAction.CallbackContext contextEvolve) 
+    {
+
+        if (contextEvolve.canceled == true) 
+        {
+            eCount++;
+            if (eCount <= 3)
+            {
+                pSpeed += 1;
+                Debug.Log("Trying to Evolve!" + eCount);
+            }
+            else 
+            { 
+            //do nothing
+            }
+        }
+        
+    }
+
+    public void OnAttack(InputAction.CallbackContext contextAttack)
+    {
+        int aCount = 0;
+
+        if (contextAttack.started == true)
+        {
+            pAnimator.SetTrigger("CatAttack1");
+            aCount++;
+            Debug.Log("attacking" + aCount);
+            pSFX.Play();
+            //pSpeed += 1;
+        }
+
     }
 }
